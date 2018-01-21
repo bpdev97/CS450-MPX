d15caf4c7b228997d1a41b0f21500fe4212527d8
Yasmine Pourarian <ydpourarian@mix.wvu.edu>
2018-01-20 23:19:25 -0500

    The Commands (not complete)

--- modules/processcommand.c
+++ modules/processcommand.c
@@ -1,14 +1,17 @@
 
 #include <string.h>
 #include <core/serial.h>
+#include <core/io.h>
 #include "processcommand.h"
 
 void help (int argc, char* argv[]) {
 
+    //checking number of user inputs
     if (argc == 1) {
         serial_println("To use the help command type 'help' followed by the command you would like to use.");
         serial_println("The commands you can use include: Help, Shutdown, and Date and Time.");
-    } else if (argc == 2) {
+    }
+    else if (argc == 2) {
         if (strcmp(argv[1], "shutdown") == 0) {
             serial_println("The Shutdown command shuts down the computer system.");
         } else if (strcmp(argv[1], "setdate") == 0) {
@@ -27,4 +30,56 @@ void help (int argc, char* argv[]) {
     else{
         serial_println("Invalid input, too many arguments");
     }
+}
+
+void shutdown (int argc, char* argv[]) {
+    serial_println("Are you sure you want to shut down?");
+}
+
+void settime (int argc, char* argv[]) {
+    cli();
+
+    char* hour = strtok(argv[1], ":");
+    outb(0x70, 0x04);
+    outb(0x71, atoi(hour); //TODO convert to BCD
+
+    char* minute = strtok(NULL, ":");
+    outb(0x70, 0x02);
+    outb(0x71, atoi(minute)); //TODO convert to BCD
+
+    char* second = strtok(NULL, ":");
+    outb(0x70, 0x00);
+    outb(0x71, atoi(second)); //TODO convert to BCD
+
+    sti();
+}
+
+void setdate (int argc, char* argv[]) {
+    cli();
+
+    char* dayofmonth = strtok(argv[1], "/");
+    outb(0x70, 0x07);
+    outb(0x71, atoi(dayofmonth)); //TODO convert to BCD
+
+    char* month = strtok(NULL, "/");
+    outb(0x70, 0x08);
+    outb(0x71, atoi(month)); //TODO convert to BCD
+
+    char* year = strtok(NULL, "/");
+    outb(0x70, 0x09);
+    outb(0x71, atoi(year)); //TODO convert to BCD
+
+    sti();
+}
+
+void getdate (int argc, char* argv[]) {
+    char* dayofmonth = argv[1];
+    outb(0x70, 0x07);
+    inb(0x71);
+}
+
+void gettime (int argc, char* argv[]) {
+    char *hour = argv[1];
+    outb(0x70, 0x04);
+    inb(0x71);
 }
--- modules/processcommand.h
+++ modules/processcommand.h
@@ -3,5 +3,10 @@
 #define MPX_SPRING2018_ABOVE_C_LEVEL_PROCESSCOMMAND_H
 
 void help (int numword, char* args[]);
+void shutdown (int argc, char* argv[]);
+void settime (int argc, char* argv[]);
+void setdate (int argc, char* argv[]);
+void getdate (int argc, char* argv[]);
+void gettime (int argc, char* argv[]);
 
 #endif //MPX_SPRING2018_ABOVE_C_LEVEL_PROCESSCOMMAND_H
