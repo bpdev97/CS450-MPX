97b678f6bdbb03c67011e26c59b11301df3cf6ce
Joseph Smith <jdsmith4@mix.wvu.edu>
2018-01-27 20:07:00 -0500

    Major fixes to polling.c

--- modules/polling.c
+++ modules/polling.c
@@ -10,18 +10,23 @@
   Description..: 
   Params..: 
 */
+/*
+TO BE DONE
+  Need to fix console line for delete and backspace
+  Probably need some more error handling
+  Delete and insert is actually bigger than a byte which is a problem we need to discuss
+  New line implementation?
+*/
 
 int poll(char * buffer, int* count) {
-	char newBuffer[100]; //size will be alocated later
 	char letter[4];
 	int counter = 0;
 	int maxBufferSize = *count;
-	int exit =0;
 	int enter = 0;
-	char emptyChar = ' ';
 	char backspace[] = {'\b',' ','\b','\0'};
         int leftCount = 0;
 	int rightCount = 0;
+
 
 	while(!enter) {  
     	if (inb(COM1+5)&1) { // Check for character
@@ -29,124 +34,124 @@ int poll(char * buffer, int* count) {
             letter[1] = inb(COM1);
             letter[2] = inb(COM1);
             letter[3] = '\0';
-
 			// Look for special case characters
-			if ( (letter[0] != BACKSPACE) && (letter[0] != ENTER) && (letter[0] != '\033') &&  (letter[0] != '~') )
+			if ( (letter[0] != BACKSPACE) && (letter[0] != ENTER) && (letter[0] != '\033') &&  (strcmp(letter, "~\000\000") != 0) && (counter < maxBufferSize))
 			 {
 				if((leftCount-rightCount) == 0)
 				{
-				   newBuffer[++counter] = letter[0]; // Add character to buffer and then increment counter
-				   // LOOK AT THIS LATER SOMETHING ISNT RIGHT... BUFFER NEEDS TO BE INIT TO NULL TERM
-				   // This will be done in the command handler and when we pass the buffer in
-				   newBuffer[++counter] = '\0'; // adds null terminate
-				
+				   
+				   buffer[counter] = letter[0]; // Add character to buffer and then increment counter
+ 			           counter++;//increase counter
 				   int count = 4;
 				   sys_req(WRITE, COM1, letter, &count); // Write the character to COM1
 				}
 
 				else if((leftCount - rightCount) > 0)//Cursor is moved to the left
 				{
-				   int counterSaver = counter;// this was added because the algorithm changes counter to something unexpected, not sure why but this is a temp fix
+				   
  		 		   counter -= ((leftCount - rightCount)); //moves pointer to null terminator left of char to be inserted
-		 		   char temp1 =  newBuffer[++counter]; //saves character being overwritten by new character
+		 		   char temp1 =  buffer[counter]; //saves character being overwritten by new character
 		 		   char temp2; 			    //saves the next character in the buffer
-		 		   newBuffer[counter] = letter[0]; // writes new letter overtop of the char saved in temp1 
+		 		   buffer[counter] = letter[0]; // writes new letter overtop of the char saved in temp1 
 		 		   int i;
-				  
-				   for(i = 0; i<(leftCount - rightCount); i+=2)
+				   for(i = 0; i<(leftCount - rightCount); i++)
 		                   {
-		    			 counter += 2; // moves ptr over to next character
-		    			 temp2 = newBuffer[counter]; //saves next character in sequence
-		    			 newBuffer[counter] = temp1; // replaces character with char saved in temp 1
+		    			 counter++; // moves ptr over to next character
+		    			 temp2 = buffer[counter]; //saves next character in sequence
+		    			 buffer[counter] = temp1; // replaces character with char saved in temp 1
 		     			 temp1 = temp2; // temp 1 equal temp 2
 		  		   }
-			          newBuffer[++counter] = temp1;//adds last character
-				  newBuffer[++counter] = '\0';
+				  counter++;
+			          buffer[counter] = temp1;//adds last character
 				  
-  				  counter = counterSaver +2; //sets counter to what counter should be 
 			          int count = 4;
 				  sys_req(WRITE, COM1, letter, &count); // Write the character to COM1
-		  		  
 				}
 				
 			}
 
 			// Handle special case characters
-			else {
+			else if(counter != 0){
 				// Enter Recieved
 				if(strcmp(letter, "\r\000\000") == 0) {
-					int i = 0;
-					serial_println("");
-
-					for(i = 0; i < counter; i++) {
-						sys_req(WRITE, COM1, &newBuffer[i], &counter);
-					}
-					enter = 1;                                 
+					
+					   int i = 0;
+					   serial_println("");
+					   buffer[counter] = '\0';
+					   enter = 1;     
+					                          
 				}
 					
 				// Backspace Recieved
-				else if(strcmp(letter, "\177\000\000") == 0) {			
-					newBuffer[counter--] = emptyChar; // @Joe can you explain why we have to do this twice
-					newBuffer[counter--] = emptyChar; // I'm a bit confused on that
+				else if(strcmp(letter, "\177\000\000") == 0) {	
+					if((leftCount - rightCount) == 0)
+					{ 
+				          counter--;
+					  buffer[counter] = '\0'; 
+					}
+					else if((leftCount - rightCount) > 0)
+					{
+			                 counter -= ((leftCount - rightCount)+1); //moves pointer to  char that is to be deleted
+		 		   	 int i;
+				  	 for(i = 0; i<(leftCount - rightCount); i++)
+		                  	 {
+					   buffer[counter] = buffer[counter+1];//shifts characters to the left
+		    		           counter++;
+		  		   	 }
+					}
 					int backCount = 4;
 					sys_req(WRITE, COM1, &backspace, &backCount);
 				}
 				
 				// Delete recieved
 				else if (strcmp(letter, "\033[3") == 0) {
-					//serial_print("\nDelete recieved!");
+					 
+					//delete is bigger than 4 long "\033[3" and "~\000\000"
+					//insert is "\033[3" and ";2~"
 
-					//this caused a lot of problems in memory that I can't explain
-					/*int count = 4;
-					sys_req(WRITE, COM1, letter, &count); // Write letter to COM1*/
-					
-					//works for the most part except it adds an extra space at the end of the buffer and im not sure why
+
+					/*int count = 4; //Does not work
+					sys_req(WRITE, COM1, letter, &count);*/
 					if((leftCount - rightCount) > 0)
 					{
-					
-			                 counter -= ((leftCount - rightCount)-1); //moves pointer to  char that is to be deleted
+			                 counter -= ((leftCount - rightCount)); //moves pointer to  char that is to be deleted
 		 		   	 int i;
-				  	 for(i = 0; i<((leftCount - rightCount)); i+=2)
+				  	 for(i = 0; i<((leftCount - rightCount)); i++)
 		                  	 {
-						newBuffer[counter] = newBuffer[counter+2];
-		    			 	counter+=2;
+						buffer[counter] = buffer[counter+1]; //moves characters to the left
+		    			 	counter++;
 		  		   	 }
-					counter-=2;//move back 2 to delete last character
-			          	newBuffer[counter] = emptyChar;//clears last character
-				        newBuffer[counter++] = emptyChar;//clears last null terminator
-				        
+					  counter--;	
+					  leftCount--;//moves cursor to the right one
 					}
-
-					
 				}
 
 				// Left arrow Key recieved
 				else if (strcmp(letter, "\033[D") == 0) {
-					int count = 4;
-					sys_req(WRITE, COM1, letter, &count); // Write letter to COM1
+					
 					if((leftCount-rightCount) < counter)//will not go before the buffer starts
 					{
-			   		   leftCount +=2;
+			   		   leftCount ++;
+					   int count = 4;
+					   sys_req(WRITE, COM1, letter, &count); // Write letter to COM1
  					}
 				}
-
 				// Right arrow Key recieved
 				else if (strcmp(letter, "\033[C") == 0) {
-					//serial_print("\nRight arrow recieved!");
-					int count = 4;
-					sys_req(WRITE, COM1, letter, &count); // Write letter to COM1
 					if((leftCount-rightCount) > 0)//will not go past the end of the string
 					{
-		       	  		   rightCount+=2;
+		       	  		   rightCount++;
+					   int count = 4;
+					   sys_req(WRITE, COM1, letter, &count); // Write letter to COM1
 					}
-					// counter++; // Move buffer counter to the right
+					
 				}
 			}
 		}
 	}
 	
 	// Enter pressed
-	count = &counter;   
-	buffer = newBuffer;
+	count = &counter;  
 	return 0;
 }
+
