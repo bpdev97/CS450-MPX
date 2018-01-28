61cf34e2a0761871836d37e7c4054e3f6f1594ea
Joseph Smith <jdsmith4@mix.wvu.edu>
2018-01-28 17:05:49 -0500

    Polling done, error testing still needed

--- modules/polling.h
+++ modules/polling.h
@@ -5,11 +5,13 @@
 
 #define ENTER 13
 #define BACKSPACE 127
-
+#define TAB 9
 /*
   Procedure..: poll
   Description..: poll from character register to get inpit
   Params..: buffer pointer and count pointer
 */
 int poll(char * buffer, int* count);
+void clearCommandLine(int counter, int cursorPosition);
+void resetCursor(int cursorPosition);
 #endif
