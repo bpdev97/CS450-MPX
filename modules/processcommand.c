9c293ae49ef753b770eaae5191272bf3ef9e8390
aaheavner <aaheavner@mix.wvu.edu>
2018-02-22 01:00:49 -0500

    Added more error checking.

--- modules/processcommand.c
+++ modules/processcommand.c
@@ -438,74 +438,112 @@ void showAll(){
 //Finds a PCB and sets its state to blocked, then reinserts
 int blockPCB(int argc, char *argv[]){
     PCB *find = FindPCB(argv[1]);
-    if(!find){
+    if(argc == 1){
+        println("Error: no process name given.");
+        return 0;
+    }
+
+    else if(!find){
         println("PCB not found.");
         return 0;
     }
 
-    RemovePCB(find);
-    find -> readyState = -1;
-    InsertPCB(find);
-    return 1;
+    else{
+        RemovePCB(find);
+        find -> readyState = -1;
+        InsertPCB(find);
+        return 1;
+    }
 }
 
 //Finds a PCB and sets its state to unblocked, then reinserts
 int unblockPCB(int argc, char *argv[]){
     PCB *find = FindPCB(argv[1]);
-    if(!find){
+    if(argc == 1){
+        println("Error: no process name given.");
+        return 0;
+    }
+
+    else if(!find){
         println("PCB not found.");
         return 0;
     }
 
-    RemovePCB(find);
-    find -> readyState = 1;
-    InsertPCB(find);
-    return 1;
+    else{
+        RemovePCB(find);
+        find -> readyState = 1;
+        InsertPCB(find);
+        return 1;
+    }
 }
 
 //Finds a PCB and sets its state to suspended, then reinserts
 int suspendPCB(int argc, char *argv[]){
     PCB *find = FindPCB(argv[1]);
-    if(!find){
+    if(argc == 1){
+        println("Error: no process name given.");
+        return 0;
+    }
+
+    else if(!find){
         println("PCB not found.");
         return 0;
     }
 
-    RemovePCB(find);
-    find -> suspendState = 1;
-    InsertPCB(find);
-    return 1;
+    else{
+        RemovePCB(find);
+        find -> suspendState = 1;
+        InsertPCB(find);
+        return 1;
+    }
 }
 
 //Finds a PCB and sets its state to resumed, then reinserts
 int resumePCB(int argc, char *argv[]){
     PCB *find = FindPCB(argv[1]);
-    if(!find){
+    if(argc == 1){
+        println("Error: no process name given.");
+        return 0;
+    }
+
+    else if(!find){
         println("PCB not found.");
         return 0;
     }
 
-    RemovePCB(find);
-    find -> suspendState = 0;
-    InsertPCB(find);
-    return 1;
+    else{
+        RemovePCB(find);
+        find -> suspendState = 0;
+        InsertPCB(find);
+        return 1;
+    }
 }
 
 //Finds PCB, sets priority, reinserts
 int setPriority(int argc, char *argv[]){
     PCB *find = FindPCB(argv[1]);
-    if(!find){
+    if(argc != 3){
+        println("Error: not enough inputs.");
+        return 0;
+    }
+
+    else if(!find){
         println("PCB not found.");
         return 0;
     }
 
     int p = atoi(argv[2]);
-    if(p < 0 || p > 9) return 0;
-
-    RemovePCB(find);
-    find -> priority = p;
-    InsertPCB(find);
-    return 1;
+    if(p < 0 || p > 9){
+        println("Error: invalid priority.");
+        return 0;
+    }
+    
+    else{
+        RemovePCB(find);
+        find -> priority = p;
+        InsertPCB(find);
+        return 1;
+    }
 }
 
 //int frombcd (int bcd){
