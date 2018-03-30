4c274977f1da454e396c538df05d896612460741
aaheavner <aaheavner@mix.wvu.edu>
2018-03-30 02:37:39 -0400

    Did work on allocMem.

--- modules/mcb.c
+++ modules/mcb.c
@@ -9,11 +9,20 @@ CMCB* FMCB;
 LMCB* END;
 void* start;
 
+u32int minSize = 128;
+
 int initializeHeap(int bytes){
+    //Beginning of the entire heap
     start = (void*) kmalloc(bytes + sizeof(CMCB) + sizeof(LMCB));
 
+    if(start == NULL){
+        return -1; //Error code
+    }
+
+    //Allocated list head initialized
     AMCB = NULL;
 
+    //Free list head initialized
     FMCB -> type = 0;
     FMCB = start;
     FMCB -> size = bytes;
@@ -26,5 +35,29 @@ int initializeHeap(int bytes){
     END -> size = bytes;
 
     return bytes;
+}
+
+void* allocMem(u32int size){
+    //Check for minimum size
+    if(size < minSize){
+        size = minSize;
+    }
+
+    //Make size a mulitple of 4 bytes (word)
+    int mod = size%4;
+    if(mod > 0){
+        size = size + (4 - mod);
+    }
+    
+    CMCB* current = FMCB;
+
+    while(current -> size < size + sizeof(CMCB) + sizeof(LMCB)){
+        if(current -> next == NULL){
+            return NULL;
+        }
+
+        current = current -> next;
+    }
+    
 }
 
